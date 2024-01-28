2ec0531cf12c0d413eaecc0e8837c28a4258b20d
Carlos Calgaro <carloscalgarof@gmail.com>
2024-01-28 23:53:07 +0000

    test: Refactored category build test

--- test/builders/themealdb/category_builder_test.rb
+++ test/builders/themealdb/category_builder_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class Themealdb::CategoryBuilderTest < ActiveSupport::TestCase
   def setup
     @category = build(:category)
-    @categories_array = [{
+    @categories_response = [{
       idCategory: @category.id,
       strCategory: @category.name,
       strCategoryThumb: @category.thumbnail,
@@ -10,20 +10,21 @@ class Themealdb::CategoryBuilderTest < ActiveSupport::TestCase
     }]
   end
 
-  test 'it builds a category object from a given array of hashes' do
-    categories = Themealdb::CategoryBuilder.from_array(@categories_array)
-    assert_equal(1, categories.size)
-    assert(categories[0].is_a?(Category))
+  test 'it builds a Category object from a given array of hashes' do
+    categories = Themealdb::CategoryBuilder.from_array(@categories_response)
+    assert_equal 1, categories.size
+    assert_instance_of Category, categories[0]
   end
 
   test 'it converts the attributes using the ATTRIBUTES_MAP constant' do
-    categories = Themealdb::CategoryBuilde.r.from_array(@categories_array)
+    categories = Themealdb::CategoryBuilder.from_array(@categories_response)
     converted_keys = categories[0].attributes.symbolize_keys.keys
-    assert_equal(converted_keys.sort, Themealdb::Mappers::Categories::ATTRIBUTES_MAP.keys.sort)
+    expected_keys = Themealdb::Mappers::Categories::ATTRIBUTES_MAP.keys
+    assert_equal expected_keys.sort, converted_keys.sort
   end
 
-  test 'it correctly maps the attributes values to the new attribute names' do
-    categories = Themealdb::CategoryBuilder.from_array(@categories_array)
-    assert_equal(categories[0].attributes, @category.attributes)
+  test 'it correctly maps the attribute values to the new attribute names' do
+    categories = Themealdb::CategoryBuilder.from_array(@categories_response)
+    assert_equal @category.attributes, categories[0].attributes
   end
 end
